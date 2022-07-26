2fe3d0704005d1246bdb147943a7c0fa1e0c83a4
danyllosoareszup <109121059+danyllosoareszup@users.noreply.github.com>
2022-07-26 14:27:46 -0300

    Add files via upload

---
+++ zupflixdojo/src/main/java/com/dojo/zupflixdojo/ZupflixdojoApplication.java
@@ -0,0 +1,13 @@
+package com.dojo.zupflixdojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZupflixdojoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZupflixdojoApplication.class, args);
+	}
+
+}
---
+++ zupflixdojo/src/test/java/com/dojo/zupflixdojo/ZupflixdojoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dojo.zupflixdojo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZupflixdojoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
